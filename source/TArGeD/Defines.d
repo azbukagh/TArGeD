278ab276e3ef2f720db277f4e684860cdfdd86f8
Azbuka <azbukapublic@yahoo.com>
2016-10-07 22:22:42 +0200

    TArGeD.Defines fully documented

--- source/TArGeD/Defines.d
+++ source/TArGeD/Defines.d
@@ -112,15 +112,21 @@ struct TGAVersion {
 	float Number;
 	char Letter;
 
+	/**
+	*	Version constructor
+	*/
 	this(ushort n, char l) {
 		this.Number = cast(float) n / 100;
 		this.Letter = l;
 	}
-
+	/// ditto
 	this(ref File f) {
 		this(f.readFile!(ushort), f.readFile!(char));
 	}
 
+	/**
+	*	Returns: formatted version
+	*/
 	string toString() {
 		auto a = appender!string();
 		a.formattedWrite("%.2f%s",
@@ -130,9 +136,25 @@ struct TGAVersion {
 	}
 }
 
+/**
+*	Represents aspect ratio
+*/
 struct TGARatio {
+	/**
+	*	Aspect ratio represented as Numerator:Denominator
+	*	---
+	*	Numerator = 16;
+	*	Denominator = 9;
+	*	// Aspect ratio is 16:9
+	*	---
+	*/
 	ushort Numerator;
+	/// ditto
 	ushort Denominator;
+
+	/**
+	*	Returns: formatted aspect ratio
+	*/
 	string toString() {
 		auto a = appender!string();
 		a.formattedWrite("%d:%d",
@@ -141,14 +163,33 @@ struct TGARatio {
 		return a.data;
 	}
 
-	bool isEnabled() {
+	/**
+	*	Returns: `false` if aspect ratio value not presented and should not be used
+	*/
+	bool isPresented() {
 		return this.Denominator != 0;
 	}
 }
 
+/**
+*	Represents gamma value
+*/
 struct TGAGamma {
+	/**
+	*	Gamma represented as Numerator:Denominator
+	*	---
+	*	Numerator = 9;
+	*	Denominator = 8;
+	*	// Gamma is 9.8
+	*	---
+	*/
 	ushort Numerator;
+	/// ditto
 	ushort Denominator;
+
+	/**
+	*	Returns: formatted gamma value
+	*/
 	string toString() {
 		auto a = appender!string();
 		a.formattedWrite("%d.%d",
@@ -157,32 +198,42 @@ struct TGAGamma {
 		return a.data;
 	}
 
+	/**
+	*	Returns: `false` if gamma value not presented and should not be used
+	*/
 	bool isEnabled() {
 		return this.Denominator != 0;
 	}
 
+	/**
+	*	Gamma have to be in range of 0.0 to 10.0
+	*	Returns: `false` if gamma value is not correct
+	*/
 	bool isCorrect() {
 		return (this.Numerator <= 10 && this.Denominator == 0) ||
 			(this.Numerator < 10);
 	}
 }
 
+/**
+*	Represents extension area
+*/
 struct TGAExtensionArea {
-	ushort Size;
-	char[40] AuthorName;
-	char[80][4] AuthorComments;
-	DateTime Timestamp;
-	char[40] JobName;
-	TimeOfDay JobTime;
-	char[40] SoftwareID;
-	TGAVersion SoftwareVersion;
-	uint KeyColor;
-	TGARatio AspectRatio;
-	TGAGamma Gamma;
-	uint ColorCorrectionOffset;
-	uint PostageStampOffset;
-	uint ScanLineOffset;
-	ubyte AttributesType; // TODO: handle it
+	ushort Size;	/// Size of extension area. Should be set to 495
+	char[40] AuthorName;	/// Author of image
+	char[80][4] AuthorComments;	/// comments in 4 lines of 80 characters
+	DateTime Timestamp;	/// Date and time when image was saved
+	char[40] JobName;	/// Job name or ID
+	TimeOfDay JobTime;	/// Job elapsed time when image was saved
+	char[40] SoftwareID;	/// Name of program, which created this image
+	TGAVersion SoftwareVersion;	/// Version of program
+	uint KeyColor;	/// Background color. TODO
+	TGARatio AspectRatio;	/// Aspect ratio
+	TGAGamma Gamma;	/// Gamma value
+	uint ColorCorrectionOffset;	/// Offset of color correction table
+	uint PostageStampOffset;	/// Offset of postage stamp image
+	uint ScanLineOffset;	/// Offset of scan line table
+	ubyte AttributesType; /// Type of alpha channel
 }
 
 /*
@@ -198,9 +249,14 @@ struct TGADeveloperArea {
 }
 */
 
+/**
+*	Represents a single pixel
+*/
 struct Pixel {
+	/// Red, Green, Blue and Alpha
 	ubyte R, G, B, A;
 
+	/// Pixel constructor
 	this(in ubyte[] data) {
 		switch(data.length) {
 			case 4:
@@ -219,28 +275,28 @@ struct Pixel {
 				break;
 		}
 	}
-
-	void from32(in ubyte[4] data, ) {
+	/// ditto
+	void from32(in ubyte[4] data) {
 		this.R = data[2];
 		this.G = data[1];
 		this.B = data[0];
 		this.A = data[3];
 	}
-
+	/// ditto
 	void from24(in ubyte[3] data) {
 		this.R = data[2];
 		this.G = data[1];
 		this.B = data[0];
 		this.A = 0xFF;
 	}
-
+	/// ditto
 	void from16(in ubyte[2] data) {
 		this.R = (data[1] & 0x7c) << 1;
 		this.G = ((data[1] & 0x03) << 6) | ((data[0] & 0xE0) >> 2);
 		this.B = (data[0] & 0x1F) << 3;
 		this.A = (data[1] & 0x80) ? 0xFF : 0;
 	}
-
+	/// ditto
 	void from8(in ubyte[1] data) {
 		this.R = data[0];
 		this.G = data[0];
