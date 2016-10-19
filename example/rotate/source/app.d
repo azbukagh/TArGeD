37a782fdf69f779368225459554f190f5b6a41ac
Azbuka <azbukapublic@yahoo.com>
2016-10-19 21:04:52 +0200

    Rotation example finished

--- example/rotate/source/app.d
+++ example/rotate/source/app.d
@@ -10,14 +10,33 @@ void main(string[] args) {
 	}
 
 	auto i = new Image(args[1]);
+	Pixel[][] o = new Pixel[][](i.Height, i.Width);
+	Pixel[] cpy;
 	switch(args[3]) {
 		case "180":
 			reverse(i.Pixels);	// Just reverse all pixel data
 			i.write(args[2]);
 			break;
 		case "90":
+			for(size_t w = 0; w < i.Width; w++)
+				for(size_t h = 0; h < i.Height; h++)
+					o[h][w] = i.Pixels(w, h);
+
+			foreach(ref k; o)
+				cpy ~= k;
+			i.Pixels = cpy;
+			reverse(i.Pixels);
+			i.write(args[2]);
 			break;
 		case "-90":
+			for(size_t w = 0; w < i.Width; w++)
+				for(size_t h = 0; h < i.Height; h++)
+					o[h][w] = i.Pixels(w, h);
+
+			foreach(ref k; o)
+				cpy ~= k;
+			i.Pixels = cpy;
+			i.write(args[2]);
 			break;
 		default:
 			writeln("Wrong angle");
